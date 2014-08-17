3ebf3d0bbed2394bc421ca2717775b147ca5f2ac
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-08-17 15:38:35 +0200

    add error messages

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var mapper = require('./src/mapper');
  */
 module.exports = function(config) {
   var result = q.defer;
-  var cnx = new couchbase.Connection(config, function(err) {
+  new couchbase.Connection(config, function(err) {
     if (err) {
       result.reject(err);
     } else {
@@ -14,19 +14,28 @@ module.exports = function(config) {
        * The couchbase orm
        */
       result.resolve({
+        cb: this;
         mappers: {},
         /**
          * Declare a new mapper
          */
-        declare: function(ns, options) {
-          mappers[ns] = mapper(options);
-          return mappers[ns];
+        declare: function(namespace, options) {
+          if (this.mappers.hasOwnProperty(namespace)) {
+            throw new Error(
+              'Namespace [' + namespace + '] is already defined !'
+            );
+          }
+          this.mappers[namespace] = mapper(this, namespace, options);
+          return this.mappers[namespace];
         },
         /**
          * Gets a mapper
          */
-        get: function(ns) {
-          return this.mappers[ns];
+        get: function(namespace) {
+          if (!this.mappers.hasOwnProperty(namespace)) {
+            throw new Error('Namespace [' + namespace + '] is undefined !');
+          }
+          return this.mappers[namespace];
         }
       });
     }
