dcb66eeb336af9a707aedc607a6a1ecbdb280655
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-08-29 23:53:17 +0200

    rewrite the mapper to handle more options and use event emitter

--- src/mapper.js
+++ src/mapper.js
@@ -1,147 +1,201 @@
 var extend = require('extend');
 var q = require('q');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var uuid = require('node-uuid');
+
 /**
- * Defines a mapper
+ * Initialize a mapper class for the specified manager
  */
-module.exports = function(manager, namespace, options) {
-  var mapper = {
-    options: extend(true,
-      // default options
+module.exports = function(manager) {
+  /**
+   * Defines a new mapper instance into the specified manager
+   */
+  var mapper = function(namespace, options) {
+    var self = this;
+    // parent constructor call
+    EventEmitter.call(this);
+    // default options
+    this.options = extend(true,
       {
         type:           namespace,
         autoincrement:  true,
         fields:         {},
         views:          {},
-        record:         {
-          beforeSave: function() { return true; }
-          ,afterSave: function() { return true; }
-          ,beforeRemove: function() { return true; }
-          ,afterRemove: function() { return true; }
+        // register factories
+        factory: {
+          field:        manager.options.factory.field(manager, this),
+          record:       manager.options.factory.record(manager, this),
+          resultset:    manager.options.factory.resultset(manager, this)
         }
       }
       , options
-    ),
-    /**
-     * Finds data from the specified resultset
-     */
-    find: function(view, criteria) {
-      var result = q.defer();
-      if (criteria instanceof Array) {
-        if (criteria.length > 1) {
-          criteria = {
-            keys: criteria
-          };
+    );
+    // initialize fields properties
+    for(var name in this.options.fields) {
+      this.options.fields[name] = new this.options.factory.field(
+        name, this.options.fields[name]
+      );
+      // behaviour decorator
+      for(var i = 0; i < manager.options.behaviours.length; i++) {
+        this.options.fields[name] = manager.options.behaviours[i].apply(
+          manager, [
+            this, this.options.fields[name], name
+          ]
+        );
+      }
+    }
+    // initialize views
+    for(var name in this.options.views) {
+      var view = this.options.views[name];
+      // handle map function
+      if (!view.hasOwnProperty('map')) {
+        view.map = 
+          'function(doc, meta) {\n'
+          + '\tif(doc._type && doc._type === ' + JSON.stringify(mapper.options.type) + ') {\n\t\t'
+        ;
+        if (view.fields.length > 1) {
+          view.map += 'emit([doc.' + view.fields.join(', doc.') + '], null);';
         } else {
-          criteria = {
-            key: criteria[0]
-          };
+          view.map += 'emit(doc.' + view.fields[0] + ', null);';
         }
-      } else if (!(criteria instanceof Object)) {
+        view.map += '\n\t}\n}';
+      } else if (view.map instanceof Function) {
+        view.map = view.map.toString();
+      }
+      // handle finder
+      if (view.find instanceof Function) {
+        this[name] = view.find;
+      } else {
+        (function(viewName) {
+          self[viewName] = function() {
+            return self.find(viewName, arguments.length > 1 ? arguments : arguments[0]);
+          };
+        })(name);
+      }
+    }
+    // chain error to manager
+    this.on('error', function(err) {
+      manager.emit('error', err);
+    });
+    // registers itself into the manager
+    manager.mappers[namespace] = this;
+    manager.emit('declare', this);
+  };
+  util.inherits(mapper, EventEmitter);
+  
+  /**
+   * Finds data from the specified resultset
+   */
+  mapper.prototype.find = function(view, criteria) {
+    var result = q.defer();
+    // handling criteria parameters
+    if (criteria instanceof Array) {
+      if (criteria.length > 1) {
         criteria = {
-          key: criteria
+          keys: criteria
+        };
+      } else {
+        criteria = {
+          key: criteria[0]
         };
       }
-      criteria = extend(true, {
-        limit: 10,
-        include_docs: true,
-        skip: 0
-      }, criteria);
-      manager.cb.view(namespace, view, criteria).query(function(err, data, misc) {
-        if (err) {
-          result.reject(err);
-        } else {
-          result.resolve(
-            resultset.deserialize(view, criteria, data, misc)
-          );
-        }
-      });
-      return result.promise
-    },
-    /**
-     * Creates a new record
-     */
-    create: function(doc) {
-      return record.deserialize(doc);
-    },
-    /**
-     * Automatically creates 
-     */
-    setup: function() {
-      var result = q.defer();
-      var docs = { views: {} };
-      var found = false;
-      for(var viewName in mapper.options.views) {
-        var view = mapper.options.views[viewName];
-        docs.views[viewName] = {};
-        if (view.hasOwnProperty('map')) {
-          found = true;
-          docs.views[viewName].map = view.map;
-        }
-        if (view.hasOwnProperty('reduce')) {
-          found = true;
-          docs.views[viewName].reduce = view.reduce;
-        }
+    } else if (!(criteria instanceof Object)) {
+      criteria = {
+        key: criteria
+      };
+    }
+    // set criteria default options :
+    criteria = extend(true, {
+      limit: 10,            // by default limit to 10 first entries
+      include_docs: true,   // also retrieve document contents to populate records
+      skip: 0               // pagination
+    }, criteria);
+    // request :
+    manager.cb.view(this.options.type, view, criteria).query(function(err, data, misc) {
+      if (err) {
+        result.reject(err);
+        self.emit('error', err);
+      } else {
+        var resultset = new this.options.factory.resultset(
+          data, {
+            view: view,
+            misc: misc,
+            criteria: criteria
+          }
+        );
+        result.resolve(resultset);
+        self.emit('find', resultset);
       }
-      if (found) {
-        manager.cb.setDesignDoc(mapper.options.type, docs, function(err) {
+    });
+    return result.promise;
+  };
+  /**
+   * Creates a new record
+   */
+  mapper.prototype.create = function(doc) {
+    return new this.options.factory.record(doc);
+  };
+  /**
+   * Retrieves the next sequence id
+   */
+  mapper.prototype.nextId = function() {
+    var result = q.defer();
+    var self = this;
+    if (this.options.autoincrement == true) {
+      manager.cb.incr(
+        'seq.' + mapper.options.type, 
+        {initial: 1, offset: 1},
+        function(err, data) {
           if (err) {
             result.reject(err);
+            self.emit('error', err);
           } else {
-            result.resolve(docs);
+            result.resolve(data.value);
+            self.emit('next', data.value);
           }
-        });
-      } else result.resolve(false);
-      return result.promise;
-    }
-  };
-  // initialize fields properties
-  for(var fieldName in mapper.options.fields) {
-    mapper.options.fields[fieldName] = require('./field')(
-      fieldName,
-      mapper.options.fields[fieldName]
-    );
-    if (mapper.options.fields[fieldName].meta.unique) {
-      mapper.options.views[fieldName] = {
-        type: 'unique', fields: [fieldName]
-      };
-    } else if (mapper.options.fields[fieldName].meta.index) {
-      mapper.options.views[fieldName] = {
-        type: 'index', fields: [fieldName]
-      };
-    }
-  }
-  // initialize views
-  for(var viewName in mapper.options.views) {
-    var view = mapper.options.views[viewName];
-    // handle map function
-    if (!view.hasOwnProperty('map')) {
-      view.map = 
-        'function(doc, meta) {\n'
-        + '\tif(doc._type && doc._type === ' + JSON.stringify(mapper.options.type) + ') {\n\t\t'
-      ;
-      if (view.fields.length > 1) {
-        view.map += 'emit([doc.' + view.fields.join(', doc.') + '], null);';
-      } else {
-        view.map += 'emit(doc.' + view.fields[0] + ', null);';
-      }
-      view.map += '\n\t}\n}';
-    } else if (view.map instanceof Function) {
-      view.map = view.map.toString();
-    }
-    // handle finder
-    if (view.find instanceof Function) {
-      mapper[viewName] = view.find;
+        }
+      );
     } else {
-      (function(viewName) {
-        mapper[viewName] = function() {
-          return this.find(viewName, arguments.length > 1 ? arguments : arguments[0]);
-        };
-      })(viewName);
+      var id = uuid.v4();
+      result.resolve(id);
+      self.emit('next', id);
     }
-  }
-  // record manager
-  var record = require('./record')(manager, mapper);
-  // resultset manager
-  var resultset = require('./resultset')(manager, mapper);
+    return result.promise;
+  };
+  /**
+   * Automatically creates views into couchbase
+   */
+  mapper.prototype.setup: function() {
+    var result = q.defer();
+    var docs = { views: {} };
+    var found = false;
+    var self = this;
+    for(var name in this.options.views) {
+      var view = this.options.views[name];
+      docs.views[name] = {};
+      if (view.hasOwnProperty('map')) {
+        found = true;
+        docs.views[name].map = view.map;
+      }
+      if (view.hasOwnProperty('reduce')) {
+        found = true;
+        docs.views[name].reduce = view.reduce;
+      }
+    }
+    if (found) {
+      manager.cb.setDesignDoc(this.options.type, docs, function(err) {
+        if (err) {
+          result.reject(err);
+          self.emit('error', err);
+        } else {
+          result.resolve(docs);
+          self.emit('setup', docs);
+        }
+      });
+    } else result.resolve(false);
+    return result.promise;
+  };
+  // returns the mapper class
   return mapper;
 };
